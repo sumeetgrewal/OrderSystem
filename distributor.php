4d500c882128fb1f3e85e9c4550d2ccc409fcd4f
Sumeet Grewal <r4l0b@lulu.ugrad.cs.ubc.ca>
2018-03-26 18:01:34 -0700

    Added filters to distributor/orders

--- distributor.php
+++ distributor.php
@@ -1,12 +1,14 @@
 <?php include 'header.php';
   
   $did = $_GET["did"];
-	
+  $rid = $_POST["rid"];
+  $sid = $_POST["sid"];
+  $status = $_POST["status"];
 	if($did) { ?>
 		<h1>Distributer # <?php echo $did ?></h1>
 	  <?php // Create connection to Oracle
 	  $conn = oci_connect("ora_r1i0b", "a16019151", "dbhost.ugrad.cs.ubc.ca:1522/ug");
-	  
+
 	  $query = 'select * from distributor where did='.$did.'';
 	  $stid = oci_parse($conn, $query);
 	  $r = oci_execute($stid);
@@ -25,11 +27,41 @@
 	     print '</tr>';
 	  }
 	  print '</tbody></table></div>'; ?>
+		<form id="filter" method="post">
+	  <hr>
+      <h5>Filter</h5>
+      <div class="form-row">
+        <div class="col"> 
+          <input class="form-control" name="rid" type="text" placeholder="RID" <?php echo(isset($_POST['rid']) ? 'value="'.$_POST['rid'].'"' : '') ?> ><br>
+        </div>
+        <div class="col">
+          <input class="form-control" name="sid" type="text" placeholder="SID" <?php echo(isset($_POST['sid']) ? 'value="'.$_POST['sid'].'"' : '') ?> ><br>
+        </div>
+        <div class="col">
+          <input class="form-control" name="status" type="text" placeholder="Status" <?php echo(isset($_POST['status']) ? 'value="'.$_POST['status'].'"' : '') ?> ><br>
+        </div>
+        <div class="form-group">
+        <input class="btn btn-primary" type="submit" value="Filter">
+      </div>
+      <hr>
+      </div>
+      </form>
+      <hr>
 	  
 	  <h1>All Orders</h1>
 	  
 	  <?php 
-	  $query2 = 'select o.oid, r.rid, r.name as restname, s.sid, s.name, o.status from orders o, supplier s, restaurant r where o.rid=r.rid AND s.sid=o.sid AND o.did='.$did.'';
+	  
+	  $query2 = 'select o.oid, o.rid, r.name as restname, s.sid, s.name, o.status from orders o, supplier s, restaurant r where o.rid=r.rid AND o.sid=s.sid AND o.did='.$did.'';
+		if ($rid) {
+	  $query2 = $query2 .'AND r.rid='.$rid;
+	  }
+	  if ($sid) {
+	  $query2 = $query2 .'AND s.sid='.$sid;
+	  }
+	  if ($status) {
+	  $query2 = $query2 .'AND o.status='.$status;
+	  }
 	  $stid2 = oci_parse($conn, $query2);
 	  $r2 = oci_execute($stid2);
 	  
